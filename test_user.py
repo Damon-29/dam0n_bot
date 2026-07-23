0ba033a1846db9ddca48c56fef49f836f5781eed
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-24 03:09:22 +0530

    Update test_user.py

--- test_user.py
+++ test_user.py
@@ -1,8 +1,12 @@
 from sources.x_client import XClient
 
 client = XClient()
+
 client.activate_guest()
 
 user_id = client.get_user_id("Wuthering_Waves")
-
 print("User ID:", user_id)
+
+timeline = client.get_user_tweets(user_id)
+
+print(type(timeline))
