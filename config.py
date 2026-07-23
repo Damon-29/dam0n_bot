48c401851ecf484e770a9fcd0c51c041aaf94b14
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-23 17:25:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+YOUTUBE_CHANNEL_ID = "UC0Bi5KMcECRVYis5Gb_ZYZQ"
 
+YOUTUBE_RSS = (
+    f"https://www.youtube.com/feeds/videos.xml?channel_id={YOUTUBE_CHANNEL_ID}"
+)
+
+WEBSITE_URL = "https://wutheringwaves.kurogames.com/en/main/news"
+
+X_USERNAME = "Wuthering_Waves"
