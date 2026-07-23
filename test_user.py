a6623ef0b581352bae09e854fc0774ff7f05c55e
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-24 03:16:03 +0530

    Update test_user.py

--- test_user.py
+++ test_user.py
@@ -1,12 +1,10 @@
-from sources.x_client import XClient
+import json
 
-client = XClient()
-
-client.activate_guest()
-
-user_id = client.get_user_id("Wuthering_Waves")
-print("User ID:", user_id)
+...
 
 timeline = client.get_user_tweets(user_id)
 
-print(type(timeline))
+with open("timeline.json", "w", encoding="utf-8") as f:
+    json.dump(timeline, f, indent=2, ensure_ascii=False)
+
+print("Saved timeline.json")
