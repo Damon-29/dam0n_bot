2d646d98904a0f9853ec88b4afa35476cfc0581c
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-23 17:39:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-from utils.storage import load
+from sources.youtube import fetch
+from utils.storage import (
+    load,
+    initialize,
+    is_initialized,
+    add_post,
+    already_posted
+)
 
-data = load()
+videos = fetch()
 
-print(data)
+if not is_initialized():
+
+    print("First run detected.")
+
+    for video in videos:
+        add_post("youtube", video)
+
+    initialize()
+
+    print(f"Stored {len(videos)} existing videos.")
+    print("Nothing will be sent to Discord.")
+
+else:
+
+    print("Checking for new videos...\n")
+
+    for video in videos:
+
+        if already_posted("youtube", video["id"]):
+            continue
+
+        print("NEW VIDEO FOUND!")
+        print(video["title"])
+
+        add_post("youtube", video)
