0e5555384436ed683227b0687eb7460df888702e
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-24 03:07:35 +0530

    Update x_constants.py

--- sources/x_constants.py
+++ sources/x_constants.py
@@ -1,5 +1,4 @@
 USER_BY_SCREEN_NAME_QUERY = "681MIj51w00Aj6dY0GXnHw"
-
 USER_TWEETS_QUERY = "RyDU3I9VJtPF-Pnl6vrRlw"
 
-DEFAULT_FEATURES = r'''PASTE_THE_ENTIRE_defaultFeatures_STRING_HERE'''
+DEFAULT_FEATURES = r'''{"android_ad_formats_media_component_render_overlay_enabled":false,...}'''
