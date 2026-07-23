84d7013b460d3e0e6a7497b449e81619372b7386
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-23 17:25:19 +0530

    Update youtube.py

--- sources/youtube.py
+++ sources/youtube.py
@@ -1,26 +1,3 @@
-import feedparser
+from config import YOUTUBE_RSS
 
-CHANNEL_ID = "UC0Bi5KMcECRVYis5Gb_ZYZQ"
-
-RSS_URL = f"https://www.youtube.com/feeds/videos.xml?channel_id={CHANNEL_ID}"
-
-def fetch():
-    feed = feedparser.parse(RSS_URL)
-
-    if not feed.entries:
-        return []
-
-    posts = []
-
-    for entry in feed.entries:
-        posts.append({
-            "id": entry.yt_videoid,
-            "title": entry.title,
-            "url": entry.link,
-            "published": entry.published,
-            "author": entry.author,
-            "thumbnail": f"https://i.ytimg.com/vi/{entry.yt_videoid}/maxresdefault.jpg",
-            "source": "YouTube"
-        })
-
-    return posts
+feed = feedparser.parse(YOUTUBE_RSS)
