55962a2960e0ce3f3a6a59d654ee914ff3e66f01
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-23 17:32:36 +0530

    Update storgae.py

--- utils/storgae.py
+++ utils/storgae.py
@@ -1,1 +1,13 @@
+import json
 
+FILE = "posted.json"
+
+
+def load():
+    with open(FILE, "r") as f:
+        return json.load(f)
+
+
+def save(data):
+    with open(FILE, "w") as f:
+        json.dump(data, f, indent=4)
