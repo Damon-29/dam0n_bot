2455f75c7074a15e789a99c0ca731c614b0a345a
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-23 18:19:33 +0530

    Update website.py

--- sources/website.py
+++ sources/website.py
@@ -1,1 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
 
+
+URL = "https://wutheringwaves.kurogames.com/en/main/news"
+
+
+def fetch():
+    response = requests.get(URL)
+
+    print("Status Code:", response.status_code)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    print(soup.prettify()[:3000])
+
+
+if __name__ == "__main__":
+    fetch()
