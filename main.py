1bd5aa2f9f4443fd50c0929774da258148f64c2d
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-23 16:56:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,10 @@ import requests
 url = "https://www.reddit.com/r/WutheringWaves/new.json?limit=5"
 
 headers = {
-    "User-Agent": "WuWaNewsBot/1.0"
+    "User-Agent": "Mozilla/5.0"
 }
 
-response = requests.get(url, headers=headers)
+response = requests.get(headers=headers, url=url)
 
 print("Status:", response.status_code)
-
-data = response.json()
-
-for post in data["data"]["children"]:
-    p = post["data"]
-
-    print("---------------------")
-    print("Title:", p["title"])
-    print("Flair:", p.get("link_flair_text"))
-    print("URL:", "https://reddit.com" + p["permalink"])
+print(response.text[:500])
