f70c1d109552fc8b6ca4c21a36936856fdd549eb
Damon-29 <116999696+Damon-29@users.noreply.github.com>
2026-07-23 17:38:53 +0530

    Update storage.py

--- utils/storage.py
+++ utils/storage.py
@@ -2,10 +2,39 @@ import json
 
 FILE = "posted.json"
 
+
 def load():
     with open(FILE, "r") as f:
         return json.load(f)
 
+
 def save(data):
     with open(FILE, "w") as f:
         json.dump(data, f, indent=4)
+
+
+def is_initialized():
+    return load()["initialized"]
+
+
+def initialize():
+    data = load()
+    data["initialized"] = True
+    save(data)
+
+
+def add_post(source, post):
+    data = load()
+
+    data[source][post["id"]] = {
+        "title": post["title"],
+        "url": post["url"],
+        "published": post["published"]
+    }
+
+    save(data)
+
+
+def already_posted(source, post_id):
+    data = load()
+    return post_id in data[source]
